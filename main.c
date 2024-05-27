116fff38158754d6d515aa11deb434e67c84e0ca
Dark-GreenCat <tranquanghuy3008@outlook.com>
2024-05-27 18:48:41 +0700

    [ADD] Add ability to run infinity

--- main.c
+++ main.c
@@ -3,79 +3,83 @@
 #include <stdlib.h>
 
 int main() {
-    printf("\nSIMPLE CALCULATOR");
+    while(1) {
+        printf("\nSIMPLE CALCULATOR");
 
-    printf("\n1. Basic Calculation");
-    printf("\n2. GCD");
-    printf("\n3. Exit");
+        printf("\n1. Basic Calculation");
+        printf("\n2. GCD");
+        printf("\n3. Exit");
 
-    printf("\nSelect option [1-3]: ");
+        printf("\nSelect option [1-3]: ");
 
 
-    uint8_t option;
-    scanf("%hhu", &option);
-    printf("\nMode selected: %hhu", option);
+        uint8_t option;
+        scanf("%hhu", &option);
+        printf("\nMode selected: %hhu", option);
 
-    if (option == 1) {
-        uint32_t first_number, second_number;
-        char operator;
+        if (option == 1) {
+            uint32_t first_number, second_number;
+            char operator;
 
-        printf("\nEnter first number: ");
-        scanf("%u", &first_number);
+            printf("\nEnter first number: ");
+            scanf("%u", &first_number);
 
-        printf("\nEnter operator [+,-,*,/]: ");
-        while (getchar() != '\n');
-        scanf("%c", &operator);
+            printf("\nEnter operator [+,-,*,/]: ");
+            while (getchar() != '\n');
+            scanf("%c", &operator);
 
-        printf("\nEnter second number: ");
-        scanf("%u", &second_number);
+            printf("\nEnter second number: ");
+            scanf("%u", &second_number);
 
-        switch (operator)
-        {
-        case '+':
-            printf("\n%u + %u = %u", first_number, second_number, first_number + second_number);
-            break;
-            
-        case '-':
-            printf("\n%u - %u = %d", first_number, second_number, first_number - second_number);
-            break;
-
-        case '*':
-            printf("\n%u * %u = %llu", first_number, second_number, (unsigned long long) first_number * second_number);
-            break;
-
-        case '/':
-            printf("\n%u / %u = %.15g", first_number, second_number, (double) first_number / second_number);
-            break;
-        
-        default:
-            break;
-        }
-    }
-    else if (option == 2) {
-        uint32_t first_number, second_number;
-
-        printf("\nEnter first number: ");
-        scanf("%u", &first_number);
-        printf("First number: %u\n", first_number);
-
-        printf("\nEnter second number: ");
-        scanf("%u", &second_number); 
-        printf("Second number: %u", second_number);
-
-        uint32_t gcd = (first_number < second_number) ? first_number : second_number;
-
-        for (;gcd >= 1;gcd--) {
-            if (first_number % gcd == 0 && second_number % gcd == 0) 
+            switch (operator)
+            {
+            case '+':
+                printf("\n%u + %u = %u", first_number, second_number, first_number + second_number);
                 break;
-        }
+                
+            case '-':
+                printf("\n%u - %u = %d", first_number, second_number, first_number - second_number);
+                break;
 
-        printf("\nGCD(%u, %u) = %u", first_number, second_number, gcd);
+            case '*':
+                printf("\n%u * %u = %llu", first_number, second_number, (unsigned long long) first_number * second_number);
+                break;
+
+            case '/':
+                printf("\n%u / %u = %.15g", first_number, second_number, (double) first_number / second_number);
+                break;
+            
+            default:
+                break;
+            }
+        }
+        else if (option == 2) {
+            uint32_t first_number, second_number;
+
+            printf("\nEnter first number: ");
+            scanf("%u", &first_number);
+            printf("First number: %u\n", first_number);
+
+            printf("\nEnter second number: ");
+            scanf("%u", &second_number); 
+            printf("Second number: %u", second_number);
+
+            uint32_t gcd = (first_number < second_number) ? first_number : second_number;
+
+            for (;gcd >= 1;gcd--) {
+                if (first_number % gcd == 0 && second_number % gcd == 0) 
+                    break;
+            }
+
+            printf("\nGCD(%u, %u) = %u", first_number, second_number, gcd);
+        }
+        else if (option == 3) {
+            printf("\nExit program!");
+            exit(0);
+        }
     }
-    else if (option == 3) {
-        printf("\nExit program!");
-        exit(0);
-    }
+
+ 
 
     return 0;
 }
